c1a6018ffd0f3d993ee6de7f633ef2aab097eb6b
Shabbir <shabbiransari421@gmail.com>
2025-10-29 21:43:42 +0530

    ,,,,

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["https://taskflow-frontend.vercel.app"],
+    origin: ["https://taskflow-frontend.vercel.app/api/"],
     credentials: true,
   })
 );
