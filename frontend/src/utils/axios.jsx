a2e58d4a7604dd86268ca877019042db5465e4ad
Shabbir <shabbiransari421@gmail.com>
2025-11-07 15:12:46 +0530

    nkfrg

--- frontend/src/utils/axios.jsx
+++ frontend/src/utils/axios.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://task-flow-backend-beige.vercel.app",
+  baseURL: "http://localhost:5000/api",
   withCredentials: true,
 });
 
